64dd148bf3d6d3d1c1a3a8c8f2c1965f56e98d62
jinalkathiriya <135434782+jinalkathiriya@users.noreply.github.com>
2024-01-07 19:21:35 +0530

    Add files via upload

---
+++ If Else Condition.py
@@ -0,0 +1,9 @@
+a=int(input("Enter your age: "))
+print("your age is: ",a)
+# conditional operators
+# >,<,>=,<=,==,!=
+
+if(a>18):
+    print("Your can drive")
+else:
+    print("You cannot drive")
