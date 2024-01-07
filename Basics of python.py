9ae9141079eaa02d276a8e4f8419ac7e450a6164
jinalkathiriya <135434782+jinalkathiriya@users.noreply.github.com>
2024-01-07 16:19:30 +0530

    Add files via upload

---
+++ Basics of python.py
@@ -0,0 +1,22 @@
+# simple statemnet
+print("hello")
+print(17*13)
+
+# Exeptions
+print("hello\"hey\" hello")
+print("hello \n hyee")
+
+# seprater
+print("hey",6,7,sep=":",end=" 0001\n")
+
+# singal line comments
+# print("hey")
+
+""" multipal 
+    line 
+    comments
+"""
+'''multipal
+    line
+    comments
+'''
---
+++ DataTypes.py
@@ -0,0 +1,26 @@
+a="jinal"
+b=123
+c=True
+d=None
+e=complex(8,2)
+b1=10
+print(a)
+print(b)
+print(b+b1)
+print("the type of a is ", type(a))
+print("the type of a is ", type(b))
+print("the type of a is ", type(c))
+print("the type of a is ", type(d))
+print(e)
+print("the type of a is ", type(e))
+
+list1=[8,9.0,[-4,5],["apple","banana"]]
+print(list1)
+
+tuple1=(("parrot","sparrow"),("lion","tiger"))
+print(tuple1)
+
+dict1={"nanme":"jinal",
+        "age":"20",
+        "canvote":True}
+ print(dict1)
