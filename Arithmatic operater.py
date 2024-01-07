0e783f9f6c684ec44e5b35b2d490b0bb5c59a732
jinalkathiriya <135434782+jinalkathiriya@users.noreply.github.com>
2024-01-07 16:21:37 +0530

    Add files via upload

---
+++ Arithmatic operater.py
@@ -0,0 +1,14 @@
+#addition
+ print(5+6)
+#substraction
+ print(15-6)
+#division
+ print(15/6)
+#multiplication
+print(5*6)
+# floor division
+print(15//6)
+#modulo
+ print(5%6)
+#squere
+ print(5**6)
