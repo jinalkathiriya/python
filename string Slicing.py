72a83add369f165f8f3f9321220936847e360686
jinalkathiriya <135434782+jinalkathiriya@users.noreply.github.com>
2024-01-07 18:19:10 +0530

    Add files via upload

---
+++ string Slicing.py
@@ -0,0 +1,13 @@
+# name="jinal,swastik"
+# print(name[0:5]) #including 0 but not 5 
+# print(0:-2)
+# print(len(name))
+
+alpha="abcdef"
+for i in alpha:
+    print(i)
+
+#Quick Quiz:
+    
+nm="jinal"
+print(nm[-4:-2]) #index -4 but no -2 (in)
