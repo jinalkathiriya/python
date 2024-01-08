af60b17bfd7a206e424197b96950c4da0a736b34
jinalkathiriya <135434782+jinalkathiriya@users.noreply.github.com>
2024-01-08 20:23:59 +0530

    Update FString.py

--- FString.py
+++ FString.py
@@ -1,6 +1,6 @@
 letter = "Hey my name is {1} and I am from {0}"
 country = "India"
-name = "Harry"
+name = "Jinal"
 
 print(letter.format(country, name))
 print(f"Hey my name is {name} and I am from {country}")
